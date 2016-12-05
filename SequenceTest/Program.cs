ddfc80e8a2c4734fd98def8650c7fda2ca27e588
Simon Nicholls <simonnicholls1@googlemail.com>
2016-12-05 13:08:46 +0000

    New files

---
+++ SequenceTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SequenceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sequence sequenceCalculator = new Sequence();
+            Console.WriteLine("Please enter an array of numbers for example 0 8 4 12 2 10 6 14:");
+            string input = Console.ReadLine();
+            List<int> integerListInput = sequenceCalculator.convertStringArrayToInt(input);
+            List<List<int>> increasingSubSequences = sequenceCalculator.calculateIncreasingSequences(integerListInput);
+            List<int> largestSubSequence = sequenceCalculator.findLargestIncreasingSequence(increasingSubSequences);
+            Console.WriteLine("The longest sub sequence is:");
+            Sequence.printCollection(largestSubSequence);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SequenceTest/Sequence.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SequenceTest
+{
+    public class Sequence
+    {
+        private List<int> _inputIntegerList;
+
+        public Sequence(string inputString)
+        {
+            _inputIntegerList = convertStringArrayToInt(inputString);
+        }
+
+        public Sequence()
+        {
+
+        }
+
+        public List<List<int>> calculateIncreasingSequences(List<int> integerList)
+        {
+            List<List<int>> increasingSubSequences = new List<List<int>>();
+            increasingSubSequences.Add(new List<int>(){integerList[0]});
+
+            for (int i = 1; i < integerList.Count; i++)
+            {
+                increasingSubSequences.Add(new List<int>());
+                List<int> currentLongestSubSequence = new List<int>();
+                for (int n = 0; n < i; n++)
+                {
+                    //Check to see if current number (i) is higher than check number (n)
+                    //Also check to see if the potential new subsequence length (+1) is greater than
+                    //the current subsequence length i.e. have we found a longer subsequence
+                    if (integerList[n] < integerList[i] && increasingSubSequences[n].Count + 1 > currentLongestSubSequence.Count)
+                    {
+                        //Create new list not just pointer to other list
+                        currentLongestSubSequence = new List<int>(increasingSubSequences[n]);
+                    }
+                }
+
+                currentLongestSubSequence.Add(integerList[i]);
+                increasingSubSequences[i] = currentLongestSubSequence;
+            }
+
+            return increasingSubSequences;
+        }
+
+        public List<int> findLargestIncreasingSequence(List<List<int>> increasingSubSequences)
+        {
+            List<int> largestSubSequence = increasingSubSequences[0];
+            int maxSubSeuquenceLength = 0;
+
+            foreach (List<int> subSequence in increasingSubSequences)
+            {
+                if (subSequence.Count > maxSubSeuquenceLength)
+                {
+                    maxSubSeuquenceLength = subSequence.Count;
+                    largestSubSequence = subSequence;
+                }
+            }
+
+            return largestSubSequence;
+        }
+
+        public List<int> convertStringArrayToInt(string listToConvert)
+        {
+            List<int> integerList = new List<int>();
+            string[] temp = listToConvert.Split(' ');
+            foreach (string element in temp)
+            {
+                integerList.Add(Convert.ToInt32(element));
+            }
+
+            return integerList;
+        }
+
+        public static void printCollection(List<int> collection)
+        {
+            foreach (int item in collection)
+            {
+                Console.Write(item + " ");
+            }
+            Console.Write("\n");
+        }
+
+        private static bool checkIfIncreasing(List<int> inputList)
+        {
+            int previousInt = 0;
+            bool increasing = true;
+
+            foreach (int item in inputList)
+            {
+                if (previousInt >= item)
+                {
+                    increasing = false;
+                }
+
+                previousInt = item;
+            }
+
+            return increasing;
+        }
+    }
+}
